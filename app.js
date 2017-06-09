b4d77822542226205b7b6eb300b3de68d81578f3
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-09 14:12:24 -0500

    variables set up for function sumAndMultiply

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ function multiply(a,b){ //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+//testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -59,7 +59,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
+  var calculatedSum = sum(a, sum(b,c));
+  var calculatedMultiply = mutiply(a,mutiply(b,c));
 
+  return[
+  ];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
