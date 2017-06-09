bbe42da3ecf521057ff6f6c57e34cf3b3d95a573
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-09 14:23:45 -0500

    calculatedSum and calculatedMultiply variables fixed inside problem3 function sumAndMultiply

--- app.js
+++ app.js
@@ -59,15 +59,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-  var calculatedSum = sum(a, sum(b,c));
-  var calculatedMultiply = mutiply(a,mutiply(b,c));
+  var calculatedSum = sum(a, parseInt(sum(b,c)[0]))[0];
+  console.log('calculatedSum :: ' + calculatedSum);
+  var calculatedMultiply = multiply(a, parseInt(multiply(b,c)[0]))[0];
+  console.log('calculatedMultiply :: ' + calculatedMultiply);
 
   return[
+    calculatedSum
+    ,calculatedMultiply
+    ,a + ' and ' + b + ' and ' + c + ' sum to ' + calculatedSum + '.'
+    ,'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + calculatedMultiply + '.'
   ];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
