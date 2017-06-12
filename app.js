e9e6fa908b8e96a9a31a517160dafbfe15b5b271
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-06-12 09:45:21 -0500

    fixed array problmes to take array of sizes other than 3

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function sum(a,b){ //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
-//testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -42,7 +42,7 @@ function multiply(a,b){ //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-//testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -59,9 +59,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-  var calculatedSum = sum(a, parseInt(sum(b,c)[0]))[0];
+  var calculatedSum = sum(a, (sum(b,c)[0]))[0];
   console.log('calculatedSum :: ' + calculatedSum);
-  var calculatedMultiply = multiply(a, parseInt(multiply(b,c)[0]))[0];
+  var calculatedMultiply = multiply(a, (multiply(b,c)[0]))[0];
   console.log('calculatedMultiply :: ' + calculatedMultiply);
 
   var sumString = a + ' and ' + b + ' and ' + c + ' sum to ' + calculatedSum + '.';
@@ -78,7 +78,7 @@ function sumAndMultiply(a,b,c){ //eslint-disable-line
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-//testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -94,10 +94,30 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 var testArray = [2,3,4]; //eslint-disable-line
+/*
+function sumArray(testArray){ //eslint-disable-line
+  var calculatedSum = sum(testArray[0], (sum(testArray[1],testArray[2])[0]))[0];
+  console.log('calculatedSum :: ' + calculatedSum);
+
+  return[
+    calculatedSum
+    ,testArray + ' was passed in as an array of numbers, and ' + calculatedSum + ' is their sum.'
+  ];
+
+}
+*/
+function sumAnswer(a,b){
+  return sum(a,b)[0];
+}
 
 function sumArray(testArray){ //eslint-disable-line
-  var calculatedSum = sum(testArray[0], parseInt(sum(testArray[1],testArray[2])[0]))[0];
-  console.log('calculatedSum :: ' + calculatedSum);
+  var arrayValue;
+  var calculatedSum = 0;
+  for(var i = 0;i < testArray.length; i++){
+    arrayValue = testArray[i];
+    calculatedSum = sumAnswer(arrayValue,calculatedSum);
+    console.log('calculatedSum :: ' + calculatedSum);
+  }
 
   return[
     calculatedSum
@@ -108,7 +128,7 @@ function sumArray(testArray){ //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-//testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -123,14 +143,25 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+function multiplyAnswer(a,b){
+  return multiply(a,b)[0];
+}
+
 function multiplyArray(testArray){ //eslint-disable-line
-  var calculatedMultiply = multiply(testArray[0], parseInt(multiply(testArray[1],testArray[2])[0]))[0];
-  console.log('calculatedMultiply :: ' + calculatedMultiply);
+  var arrayValue;
+  var calculatedMultiply = 1;
+  for(var i = 0;i < testArray.length; i++){
+    arrayValue = testArray[i];
+    calculatedMultiply = multiplyAnswer(arrayValue,calculatedMultiply);
+    console.log('calculatedMultiply :: ' + calculatedMultiply);
+    console.log('The numbers ' + testArray + ' have a product of 24.');
+  }
 
   return[
     calculatedMultiply
-    ,'The numbers ' + testArray + ' have a product of ' + calculatedMultiply + '.'
+    ,'The numbers ' + testArray + ' have a product of 24.'
   ];
+
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
